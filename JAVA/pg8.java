6e78b4889139ec853c19b917ae6668fae5c223db
MEGHARANI <meghagani21@gmail.com>
2026-01-07 07:25:55 +0530

    Add files via upload

---
+++ JAVA/pg8.java
@@ -0,0 +1,18 @@
+class Outer { 
+void display() { 
+System.out.println("This is the outer class display method."); 
+} 
+class Inner { 
+void display() { 
+System.out.println("This is the inner class display method."); 
+} 
+} 
+} 
+public class pg8 { 
+public static void main(String[] args) {
+Outer outer = new Outer(); 
+outer.display();  
+Outer.Inner inner = outer.new Inner(); 
+inner.display(); 
+} 
+}
